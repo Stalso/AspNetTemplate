05f70e693d2d021a9ef1f4c8931ea9bfcb021706
Denis Abramovich <abr98904@gmail.com>
2016-02-28 21:15:09 +0300

    ASOS added

---
+++ src/Template.WebApi/Models/ApplicationContext.cs
@@ -0,0 +1,7 @@
+﻿using Microsoft.Data.Entity;
+
+namespace Template.WebApi.Models {
+    public class ApplicationContext : DbContext {
+        public DbSet<Application> Applications { get; set; }
+    }
+}
--- src/Template.WebApi/Startup.cs
+++ src/Template.WebApi/Startup.cs
@@ -7,6 +7,13 @@ using Microsoft.AspNet.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using AspNet.Security.OpenIdConnect.Extensions;
+using Template.WebApi.Extensions;
+using NWebsec.Middleware;
+using Template.WebApi.Providers;
+using Template.WebApi.Models;
+using Microsoft.Data.Entity;
+using Microsoft.AspNet.Http;
 
 namespace Template.WebApi
 {
@@ -26,6 +33,13 @@ namespace Template.WebApi
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddEntityFramework()
+               .AddInMemoryDatabase()
+               .AddDbContext<ApplicationContext>(options => {
+                   options.UseInMemoryDatabase();
+               });
+
+            services.AddAuthentication();
             // Add framework services.
             services.AddMvc();
         }
@@ -40,7 +54,61 @@ namespace Template.WebApi
 
             app.UseStaticFiles();
 
+            // Create a new branch where the registered middleware will be executed only for API calls.
+            app.UseWhen(context => context.Request.Path.StartsWithSegments(new PathString("/api")), branch => {
+                branch.UseJwtBearerAuthentication(options => {
+                    options.AutomaticAuthenticate = true;
+                    options.AutomaticChallenge = true;
+                    options.RequireHttpsMetadata = false;
+
+                    options.Audience = "http://localhost:10450/";
+                    options.Authority = "http://localhost:10450/";
+                });
+            });
+
+
+            // Note: visit https://docs.nwebsec.com/en/4.2/nwebsec/Configuring-csp.html for more information.
+            app.UseCsp(options => options.DefaultSources(configuration => configuration.Self())
+                                         .ImageSources(configuration => configuration.Self().CustomSources("data:"))
+                                         .ScriptSources(configuration => configuration.UnsafeInline())
+                                         .StyleSources(configuration => configuration.Self().UnsafeInline()));
+
+            app.UseXContentTypeOptions();
+
+            app.UseXfo(options => options.Deny());
+
+            app.UseXXssProtection(options => options.EnabledWithBlockMode());
+
+            app.UseOpenIdConnectServer(options => {
+                options.Provider = new AuthorizationProvider();
+
+                // Note: see AuthorizationController.cs for more
+                // information concerning ApplicationCanDisplayErrors.
+                options.ApplicationCanDisplayErrors = true;
+                options.AllowInsecureHttp = true;
+
+                // Note: by default, tokens are signed using dynamically-generated
+                // RSA keys but you can also use your own certificate:
+                // options.SigningCredentials.AddCertificate(certificate);
+            });
+
             app.UseMvc();
+
+            app.UseWelcomePage();
+
+            using (var database = app.ApplicationServices.GetService<ApplicationContext>())
+            {
+                database.Applications.Add(new Application
+                {
+                    ApplicationID = "myClient",
+                    DisplayName = "My client application",
+                    RedirectUri = "http://localhost:53507/signin-oidc",
+                    LogoutRedirectUri = "http://localhost:53507/",
+                    Secret = "secret_secret_secret"
+                });
+
+                database.SaveChanges();
+            }
         }
 
         // Entry point for the application.
