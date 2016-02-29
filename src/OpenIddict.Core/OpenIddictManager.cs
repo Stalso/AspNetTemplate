b86b236b5ec08e168a9cd9c70abbdfbf6b223789
Denis Abramovich <abr98904@gmail.com>
2016-02-29 18:21:04 +0300

    OpenIddict manager started to implement

--- src/OpenIddict.Core/OpenIddictManager.cs
+++ src/OpenIddict.Core/OpenIddictManager.cs
@@ -133,5 +133,117 @@ namespace OpenIddict
 
             return identity;
         }
+
+        public virtual Task<TApplication> FindApplicationByIdAsync(string identifier)
+        {
+            return Store.FindApplicationByIdAsync(identifier, CancellationToken);
+        }
+
+        public virtual Task<TApplication> FindApplicationByLogoutRedirectUri(string url)
+        {
+            return Store.FindApplicationByLogoutRedirectUri(url, CancellationToken);
+        }
+
+        public virtual async Task<string> FindClaimAsync(TUser user, string type)
+        {
+            if (user == null)
+            {
+                throw new ArgumentNullException(nameof(user));
+            }
+
+            if (string.IsNullOrEmpty(type))
+            {
+                throw new ArgumentNullException(nameof(type));
+            }
+
+            // Note: GetClaimsAsync will automatically throw an exception
+            // if the underlying store doesn't support custom claims.
+
+            return (from claim in await GetClaimsAsync(user)
+                    where string.Equals(claim.Type, type, StringComparison.Ordinal)
+                    select claim.Value).FirstOrDefault();
+        }
+
+        public virtual async Task<string> GetApplicationTypeAsync(TApplication application)
+        {
+            if (application == null)
+            {
+                throw new ArgumentNullException(nameof(application));
+            }
+
+            var type = await Store.GetApplicationTypeAsync(application, CancellationToken);
+
+            // Ensure the application type returned by the store is supported by the manager.
+            if (!string.Equals(type, OpenIddictConstants.ApplicationTypes.Confidential, StringComparison.OrdinalIgnoreCase) &&
+                !string.Equals(type, OpenIddictConstants.ApplicationTypes.Public, StringComparison.OrdinalIgnoreCase))
+            {
+                throw new InvalidOperationException("Only 'confidential' or 'public' applications are " +
+                                                    "supported by the default OpenIddict manager.");
+            }
+
+            return type;
+        }
+
+        public virtual Task<string> GetDisplayNameAsync(TApplication application)
+        {
+            if (application == null)
+            {
+                throw new ArgumentNullException(nameof(application));
+            }
+
+            return Store.GetDisplayNameAsync(application, CancellationToken);
+        }
+
+        public virtual async Task<bool> ValidateRedirectUriAsync(TApplication application, string address)
+        {
+            if (application == null)
+            {
+                throw new ArgumentNullException(nameof(application));
+            }
+
+            if (!string.Equals(address, await Store.GetRedirectUriAsync(application, CancellationToken), StringComparison.Ordinal))
+            {
+                Logger.LogWarning("Client validation failed because {RedirectUri} was not a valid redirect_uri " +
+                                  "for {Client}", address, await GetDisplayNameAsync(application));
+
+                return false;
+            }
+
+            return true;
+        }
+
+        public virtual async Task<bool> ValidateSecretAsync(TApplication application, string secret)
+        {
+            if (application == null)
+            {
+                throw new ArgumentNullException(nameof(application));
+            }
+
+            if (!await this.IsConfidentialApplicationAsync(application))
+            {
+                Logger.LogWarning("Client authentication cannot be enforced for non-confidential applications.");
+
+                return false;
+            }
+
+            var hash = await Store.GetHashedSecretAsync(application, CancellationToken);
+            if (string.IsNullOrEmpty(hash))
+            {
+                Logger.LogError("Client authentication failed for {Client} because " +
+                                "no client secret was associated with the application.");
+
+                return false;
+            }
+
+            var hasher = new PasswordHasher<TApplication>();
+            if (hasher.VerifyHashedPassword(application, hash, secret) == PasswordVerificationResult.Failed)
+            {
+                Logger.LogWarning("Client authentication failed for {Client}.", await GetDisplayNameAsync(application));
+
+                return false;
+            }
+
+            return true;
+        }
     }
 }
